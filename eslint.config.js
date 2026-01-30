a1fed49a5d09645a2292581bc489e997d15359c5
Allysson Rodrigues <alissonrodriig@gmail.com>
2026-01-30 09:27:45 -0300

    fix: configure ESLint for Node globals

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,16 @@
 import js from '@eslint/js';
 import prettier from 'eslint-config-prettier';
 
-export default [js.configs.recommended, prettier];
+export default [
+  {
+    ...js.configs.recommended,
+    languageOptions: {
+      globals: {
+        console: 'readonly',
+        require: 'readonly',
+        module: 'readonly',
+      },
+    },
+  },
+  prettier,
+];
